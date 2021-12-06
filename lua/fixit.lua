0d8428d0d5ec0228420b082f1832c71df51574f2
Per <dubgeiser@gmail.com>
2021-12-06 11:58:37 +0100

    qflist is filled, but items still lack info
    Only the message is set, location and type, etc... will be done later.

--- lua/fixit.lua
+++ lua/fixit.lua
@@ -3,7 +3,7 @@
 --
 
 local parsers = require('nvim-treesitter.parsers')
-local ts_utils= require('nvim-treesitter.ts_utils')
+local tsutils= require('nvim-treesitter.ts_utils')
 
 -- The tokens to consider.
 local tokens = {
@@ -16,32 +16,51 @@ local tokens = {
 -- @param string The Fixit token (see tokens)
 -- @return string a string compatible with a line in qflist.
 local function to_qfline(comment)
-  -- TODO Make qflist compatible.
   -- TODO Look at qfformat, if possible, to adhere to that.
-  return 'FIXME | position |'..comment
+  -- TODO Add type and location
+  return {
+    text = comment,
+  }
 end
 
+-- @param TSNode node The comment TSNode to check...
 -- @return whether or not the given text is a FiXiT comment.
-local function is_fixit_comment(text)
+--         First found token triggers `true`
+local function is_fixit_comment(comment)
   for _, t in ipairs(tokens) do
-    if text:startswith(t) then
+    if string.match(comment, t) then
       return true
     end
   end
   return false
 end
 
+-- @return string[]  All the FIXIT comments within the given node.
+local function all_comments(node, comments)
+  if node:type() == "comment" then
+    local comment = tsutils.get_node_text(node)[1]
+    if is_fixit_comment(comment) then
+      table.insert(comments, tsutils.get_node_text(node)[1])
+    end
+  else
+    for child in node:iter_children() do
+      all_comments(child, comments)
+    end
+  end
+  return comments
+end
+
+
 --
 -- Find all the tokens and list them in the QuickFix window.
 local function list()
   local qflines = {}
-  local comments = {} -- TODO query for comments
+  local comments = all_comments(parsers.get_tree_root(), {})
   for _, comment in ipairs(comments) do
-    if is_fixit_comment(comment) then
-      table.insert(qflines, to_qfline(comment))
-    end
+    table.insert(qflines, to_qfline(comment))
   end
-  -- TODO set qflist with qflines
+  vim.fn.setqflist(qflines)
+  vim.api.nvim_command('copen')
 end
 
 local function setup()
