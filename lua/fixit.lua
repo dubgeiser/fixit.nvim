35150d79669aed18130fa5b4baeb700d8631c0f9
Per Juchtmans <per@dubgeiser.net>
2022-10-27 01:46:54 +0200

    Introducing token types
    The tokens to consider as Fixits are now grouped by type ('TODO', 'FIX',
    and so on) and will be shown in that given order in the qfwindow.
    
    Closes #5

--- lua/fixit.lua
+++ lua/fixit.lua
@@ -6,44 +6,45 @@ local ts = vim.treesitter
 local currbuf = vim.api.nvim_get_current_buf
 
 -- The tokens to consider.
+-- This maps a token type to a list of Fixit tokens.
+-- Every line in the map is a table whereby the first element denotes the type
+-- of Fixit (todo, note, fix, ...) and the second element is a table of all the
+-- Fixit nodes to consider for that type.
+-- The type will be shown in the QuickFix window, qf thinks of these as modules.
+-- Fixits are grouped by type and shown in order as they are defined here.
 -- Special chars should be properly escaped so they can be used in a treesitter
 -- #match? predicate.
-local tokens = {
-  'FIXME',
-  'FIXME:',
-  'XXX',
-  'TODO',
-  'TODO:',
-  '\\\\@todo',
+local token_map = {
+  {"FIX", {'FIXME', 'FIXME:'}},
+  {"TODO", {'TODO', 'TODO:', '\\\\@todo'}},
+  {"NOTE", {'NOTE', 'NOTE:', 'XXX', 'XXX:', '\\\\@note'}},
 }
 
--- String representation of the tokens that can be used to match against in our
--- Treesitter query.
-local tokens_query_match = table.concat(tokens, '|')
-
 -- @param string Text to parse out the Fixit token and the corresponding text.
--- @return table First element is the Fixit token, second the text without token.
-local function parse_full_comment(fulltext)
+-- @param table tokens The tokens to consider for the given type.
+-- @return table First element is the type of Fixit token, second the text.
+local function parse_full_comment(fulltext, tokens)
   local capture
   local text
   for _, token in ipairs(tokens) do
-    local literal_token = token:gsub('\\', '', 2)
-    capture = fulltext:gmatch(literal_token .. '%s(.*)$')
+    capture = fulltext:gmatch(token:gsub('\\', '', 2) .. '%s(.*)$')
     text = capture()
     if text ~=nil then
-      return literal_token, text
+      return text
     end
   end
 end
 
 -- @param node TSNode A node representing a fixit comment.
+-- @param string token_type The type of tokens we're converting
+-- @param table tokens The tokens to consider for the given type.
 -- @return table a structure, compatible with the quickfix window.
-local function node2qf(node)
+local function node2qf(node, token_type, tokens)
   local row, col, _ = node:start()
-  local token, text = parse_full_comment(ts.query.get_node_text(node, currbuf()))
+  local text = parse_full_comment(ts.query.get_node_text(node, currbuf()), tokens)
   return {
     text = text,
-    module = token,
+    module = token_type,
     lnum = row + 1,
     col = col + 1,
     valid = true,
@@ -51,16 +52,13 @@ local function node2qf(node)
   }
 end
 
+-- @param table the tokens to conside
 -- @return TSQuery The query that will collect the Fixit nodes.
-local function build_query()
-  -- TODO Capture the Fixit tokens directly in the query and put it in metadata.
-  -- TODO Parse out the text (without the Fixit token)
-  -- If both of these can be done, we remove the need for `parse_full_comment`
+local function build_query(tokens)
   return ts.parse_query(vim.bo.filetype, [[
     (
       (comment) @comment
-      (#match? @comment "]]..tokens_query_match..[[")
-      (#set! @comment "type" "Fixit")
+      (#match? @comment "]]..table.concat(tokens, '|')..[[")
     )
   ]])
 end
@@ -70,12 +68,17 @@ local function qflist()
   local language_tree = ts.get_parser(currbuf(), vim.bo.filetype)
   local syntax_tree = language_tree:parse()
   local root = syntax_tree[1]:root()
-  local query = build_query()
-
   local qflines = {}
-  for _, match, _ in query:iter_matches(root, currbuf()) do
-    for _, node in pairs(match) do
-      table.insert(qflines, node2qf(node))
+  local query, token_type, tokens
+
+  for _, map in ipairs(token_map) do
+    token_type = map[1]
+    tokens = map[2]
+    query = build_query(tokens)
+    for _, match, _ in query:iter_matches(root, currbuf()) do
+      for _, node in pairs(match) do
+        table.insert(qflines, node2qf(node, token_type, tokens))
+      end
     end
   end
 
